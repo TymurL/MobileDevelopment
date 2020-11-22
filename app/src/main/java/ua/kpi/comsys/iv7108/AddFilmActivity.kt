17990e927b3bc0a95e3493d6be8a5c9412ab5203
TymurL <logvinenko814@gmail.com>
2020-11-22 20:55:41 +0200

    fix adding new movie

--- app/src/main/java/ua/kpi/comsys/iv7108/AddFilmActivity.kt
+++ app/src/main/java/ua/kpi/comsys/iv7108/AddFilmActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -14,14 +15,18 @@ class AddFilmActivity : AppCompatActivity() {
     }
 
     fun addNewFilm(view: View) {
-        val title: EditText = findViewById(R.id.editTextTextPersonName)
-        val type: EditText = findViewById(R.id.editTextTextPersonName2)
-        val year: EditText = findViewById(R.id.editTextNumber)
-        val intent = Intent()
-        intent.putExtra("title", title.text.toString())
-        intent.putExtra("type", type.text.toString())
-        intent.putExtra("year", year.text.toString())
-        setResult(RESULT_OK, intent)
-        finish()
+        val title: String = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+        val type: String = findViewById<EditText>(R.id.editTextTextPersonName2).text.toString()
+        val year: String = findViewById<EditText>(R.id.editTextNumber).text.toString()
+        if (title.isNotEmpty() && type.isNotEmpty() && year.isNotEmpty()) {
+            val intent = Intent()
+            intent.putExtra("title", title)
+            intent.putExtra("type", type)
+            intent.putExtra("year", year)
+            setResult(RESULT_OK, intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Please fill parameters", Toast.LENGTH_SHORT).show()
+        }
     }
 }
